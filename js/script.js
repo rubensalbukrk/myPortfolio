a866e210c67ff8af44924e49ae736f424c9e4ba0
rubensalbukrk <rubensf4rda@gmail.com>
2023-04-16 19:43:00 -0300

    Conclusão de responsividade

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
-//ACESSANDO ELEMENTO
+
+//ACESSANDO ELEMENTOS DOM
 const contents = document.querySelectorAll(".contents");
 const button = document.querySelectorAll(".button");
 const skills = document.getElementById("pagskills");
 const projects = document.getElementById("pagprojects");
-const about = document.getElementById("pagabout");
+const contato = document.getElementById("pagcontato");
 const mensagem =  document.querySelector(".mensagemOn");
+const texto = document.querySelector(".texto");
+//**EMAIL */
+
+//CONSUMIR API FETCH
+const url = "https://jsonplaceholder.typicode.com/posts"
+const loadingElement = document.querySelector("#loading")
+const posts = document.querySelector("#posts-container")
+
+
+function loadingContent(){
+    loadingElement.id = "aaa"
+}
+loadingContent()
 
 const showProject = () => {
     mensagem.classList.value = "mensagemOff";
     projects.classList.value = "contents";
     skills.classList.value = "hide";
-    about.classList.value = "hide";
+    contato.classList.value = "hide";
 }
 const showSkills = () => {
     mensagem.classList.value = "mensagemOff";
    skills.classList.value = "contents";
-   about.classList.value = "hide";
+   contato.classList.value = "hide";
    projects.classList.value = "hide";
 }
 
 const showAbout = () => {
     mensagem.classList.value = "mensagemOff";
-    about.classList.value = "contents";
+    contato.classList.value = "contents";
     projects.classList.value = "hide";
     skills.classList.value = "hide";
 
@@ -30,7 +44,7 @@ const showAbout = () => {
 const xClose = () => {
     projects.classList.value = "hide";
     skills.classList.value = "hide";
-    about.classList.value = "hide";
+    contato.classList.value = "hide";
     mensagem.classList.value = "mensagemOn";
     
 }
